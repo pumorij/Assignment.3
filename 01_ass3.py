05e142ef6c36cb412519440ab5a604039d94f992
pumorij <pumori2015108@gndec.ac.in>
2022-07-19 11:50:07 +0530

    first commit

---
+++ 01_ass3.py
@@ -0,0 +1,11 @@
+# Q1
+
+def a_fun():
+    global name
+    name = 'A'
+def b_fun():
+    global name
+    name = 'B'
+b_fun()
+a_fun()
+print (name)
---
+++ 05_ass3.py
@@ -0,0 +1,12 @@
+# Q5
+
+def f():
+    x = 42
+    def g():
+            global x
+            x =43
+    print("Before calling g: ",x)
+    g()
+    print("After calling g: ",x)
+f()
+print("x in main: " ,x)
